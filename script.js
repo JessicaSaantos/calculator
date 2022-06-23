449d4d54205f5e4fe641ee1762043ffd67ed339c
Jessica <98659450+JessicaSaantos@users.noreply.github.com>
2022-06-22 21:10:14 -0300

    usando eval()

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ let resultado = document.getElementById('resultado')
 function insert(num){ 
  resultado.value = resultado.value + num
 }
+
 function clean (){
     resultado.value = ''
 }
+
 function deleteLast() {
     let res = resultado.value
     resultado.value = res.substring(0, res.length -1)
+}
+
+function calcular(){
+   let resul = resultado.value
+   if(resul) {
+    resultado.value = eval(resul)
+   } else {
+    resultado.value = 'ERRO'
+   }
 }
